7cb019919b603b89bef2bc6be7b319f53147d73d
amaryana <amarpatel1290@gmail.com>
2016-04-16 13:00:43 -0600

    updates

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,7 @@
 angular.module('cosmicdigest').controller('PostsController', function($scope, $firebaseArray, FirebaseService){
 
+
+
 $scope.getPosts = function () {
   FirebaseService.getPosts().then(function(res){
     $scope.posts = res.data;
--- js/service.js
+++ js/service.js
@@ -1,9 +1,22 @@
 angular.module('cosmicdigest').service('FirebaseService', function($http, $q){
 
+
   this.getPosts = function(posts) {
     return $http ({
        method: 'GET',
        url: 'https://cosmic-digest.firebaseio.com/posts.json'})
+  }
+
+  var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog");
+  
+  usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
   }
 
 });
